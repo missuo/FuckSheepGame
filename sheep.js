96a17c764d2510a972777066cd5c570ac4422c00
Vincent Young <u@nisekoo.com>
2022-09-20 18:25:46 +0800

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -11,7 +11,7 @@
 /*
 [rewrite_local]
 # > Fuck Sheep Game
-^https?:\/\/cat-match\.easygame2021\.com\/sheep\/v1\/game\/map_info url script-request-header https://raw.githubusercontent.com/missuo/FuckSheepGame/main/sheep.js
+^https?:\/\/cat-match\.easygame2021\.com\/sheep\/v1\/game\/map_info\? url script-request-header https://raw.githubusercontent.com/missuo/FuckSheepGame/main/sheep.js
 [mitm] 
 hostname = cat-match.easygame2021.com
 */
